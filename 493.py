071da9b9d7e05866875afd613633ffb37d7525f8
Julia Schwarz <julenka@Julias-Macbook-Pro-2.local>
2015-07-09 08:37:39 -0400

    493 solved, messy code

--- 493.py
+++ 493.py
@@ -8,54 +8,57 @@ Give your answer with nine digits after the decimal point (a.bcdefghij).
 """
 __author__ = 'julenka'
 
-from utils import euler
-from collections import Counter
-
-sets_of_balls = euler.choose(70, 20)
-print sets_of_balls
-
-def rcr(n_balls, k_colors, max_size, depth):
-    # print "{} rcr({},{},{}):".format("\t" * depth, n_balls, k_colors, max_size)
-    if n_balls == 0 and k_colors == 0:
-        return Counter({0: 1})
-
-    if n_balls == 0 or k_colors == 0:
-        # print "{} rcr({},{},{}) return {}".format("\t" * depth, n_balls, k_colors, max_size, Counter({n_balls: k_colors}))
-        return Counter({n_balls: k_colors})
-
-    if n_balls < 0:
-        raise Exception("unexpected value n_balls {}".format(n_balls))
-
-    ways = rcr(n_balls, k_colors - 1, max_size, depth + 1)
-
-    for balls_for_color in xrange(1, min(n_balls + 1, max_size + 1)):
-        if n_balls - balls_for_color < 0:
-            continue
-        ways2 = rcr(n_balls - balls_for_color, k_colors - 1, max_size, depth + 1)
-        ways3 = Counter()
-        for k, v in ways2.items():
-            ways3[k + 1] = v
-        ways += ways3
-    # print "{} rcr({},{},{}) return {}".format("\t" * depth, n_balls, k_colors, max_size, ways)
-    return ways
-
-total_ways = rcr(20, 7, 10, 0)
-print total_ways
-
-result_sum = reduce(lambda y, x: x[0] * x[1] + y, total_ways.items(), 0)
-print result_sum
-
-print float(result_sum) / sets_of_balls
-
-print float(result_sum) / sum(total_ways.values())
+# from utils import euler
+# from collections import Counter
+#
+# sets_of_balls = euler.choose(70, 20)
+# print sets_of_balls
+#
+# def rcr(n_balls, k_colors, max_size, depth):
+#     # print "{} rcr({},{},{}):".format("\t" * depth, n_balls, k_colors, max_size)
+#     if n_balls == 0 and k_colors == 0:
+#         return Counter({0: 1})
+#
+#     if n_balls == 0 or k_colors == 0:
+#         # print "{} rcr({},{},{}) return {}".format("\t" * depth, n_balls, k_colors, max_size, Counter({n_balls: k_colors}))
+#         return Counter({n_balls: k_colors})
+#
+#     if n_balls < 0:
+#         raise Exception("unexpected value n_balls {}".format(n_balls))
+#
+#     ways = rcr(n_balls, k_colors - 1, max_size, depth + 1)
+#
+#     for balls_for_color in xrange(1, min(n_balls + 1, max_size + 1)):
+#         if n_balls - balls_for_color < 0:
+#             continue
+#         ways2 = rcr(n_balls - balls_for_color, k_colors - 1, max_size, depth + 1)
+#         ways3 = Counter()
+#         for k, v in ways2.items():
+#             ways3[k + 1] = v
+#         ways += ways3
+#     # print "{} rcr({},{},{}) return {}".format("\t" * depth, n_balls, k_colors, max_size, ways)
+#     return ways
+#
+# total_ways = rcr(20, 7, 10, 0)
+# print total_ways
+#
+# result_sum = reduce(lambda y, x: x[0] * x[1] + y, total_ways.items(), 0)
+# print result_sum
+#
+# print float(result_sum) / sets_of_balls
+#
+# print float(result_sum) / sum(total_ways.values())
 
 from multiprocessing.pool import Pool
 import time
 import numpy as np
+from collections import Counter
 
+counter = Counter()
 def job_worker(args):
     num_iterations, worker_idx = args
     balls = np.array([x / 10 for x in range(70)])
+
     balls_to_pick = 20
 
     expected_value_sum = 0
@@ -63,8 +66,10 @@ def job_worker(args):
 
     for i in xrange(num_iterations):
         np.random.shuffle(balls)
+        print balls
         picked_balls = set(balls[:balls_to_pick])
         expected_value_sum += len(picked_balls)
+        counter[len(picked_balls)] += 1
         if i % 1000000 == 0:
             elapsed = time.time() - start_time
             print "worker", worker_idx, i, expected_value_sum, "elapsed", elapsed
@@ -72,16 +77,17 @@ def job_worker(args):
     return expected_value_sum
 
 
-num_workers = 10
+num_workers = 1
 
-iterations_per_worker = 100000
+iterations_per_worker = 1375400
+#
+# pool = Pool(processes=num_workers)
+# input_array = [iterations_per_worker] * num_workers
+# results = pool.map(job_worker, zip(input_array, xrange(num_workers)))
+result = job_worker((iterations_per_worker, 1))
+print result
 
-pool = Pool(processes=num_workers)
-input_array = [iterations_per_worker] * num_workers
-results = pool.map(job_worker, zip(input_array, xrange(num_workers)))
 
-for result in results:
-    print result
+print result / (iterations_per_worker * num_workers)
 
-
-print float(sum(results)) / (iterations_per_worker * num_workers)
+print counter
--- 493_2.py
+++ 493_2.py
@@ -1,4 +1,5 @@
 __author__ = 'julenka'
+from utils import euler
 
 # 70 balls
 # 7 colors, 10 per color
@@ -15,9 +16,14 @@ def rcr(colors_left, balls_left, acc, acc2):
     if balls_left < 0:
         return
     if balls_left == 0:
-        # print acc
-
-        num_colors_to_count[len(filter(lambda x: x > 0, acc.values()))] += 1
+        assert sum(acc.values()) == max_pick
+        print acc
+        num_colors2 = len(filter(lambda x: x > 0, acc.values()))
+        ways = 1
+        for _, count in acc.items():
+            ways *= euler.choose(10, count)
+        num_colors_to_count[num_colors2] += ways
+        return
         # num_colors_to_count[acc2] += 1
     if colors_left <= 0:
         return
@@ -27,15 +33,17 @@ def rcr(colors_left, balls_left, acc, acc2):
         acc[color_idx] = n
         acc3 = acc2 if n == 0 else acc2 + 1
         rcr(colors_left - 1, balls_left - n, acc, acc3)
+        acc[color_idx] = 0
 
 rcr(num_colors, max_pick, {}, 0)
 
 print num_colors_to_count
 
-s =  sum(num_colors_to_count.values())
+s = sum(num_colors_to_count.values())
 
 s2 = reduce(lambda y, x: x[0] * x[1] + y, num_colors_to_count.items(), 0)
 
 print s
 print s2
-print float(s2) / float(s)
+print euler.choose(70, 20)
+print float(s2) / euler.choose(70, 20)
