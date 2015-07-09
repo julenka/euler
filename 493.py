ec694f8480bba4fe54456f0749fbb1035ef4c867
Julia Schwarz <julenka@cs.cmu.edu>
2015-07-09 12:19:05 -0400

    493 clean up

--- 493.py
+++ 493.py
@@ -10,41 +10,46 @@ Give your answer with nine digits after the decimal point (a.bcdefghij).
 """
 __author__ = 'julenka'
 from utils import euler
+from collections import Counter
 
-num_colors = 7
-balls_per_color = 10
-max_pick = 20
-num_colors_to_count = {x: 0 for x in xrange(num_colors + 1)}
+NUM_COLORS = 7
+BALLS_PER_COLOR = 10
+NUM_BALLS_TO_PICK = 20
 
-def rcr(colors_left, balls_left, acc):
+def rcr(colors_left, balls_left, color_to_count, num_colors_to_ways):
     """ Recursively Compute how many ways to pick n colors for n in [0, balls_per_color]
 
-    :param colors_left:
-    :param balls_left:
-    :param acc:
-    :return:
+    This method modifies num_colors_to_ways.
+
+    :param colors_left: how many colors are left
+    :param balls_left: how many balls are left
+    :param color_to_count: color_idx -> # balls of that color
+    :return: None. Modifies num_colors_to_ways
     """
     if balls_left < 0:
         return
     if balls_left == 0:
-        assert sum(acc.values()) == max_pick
-        num_colors_for_set = len(filter(lambda x: x > 0, acc.values()))
-        ways = 1
-        for _, count in acc.items():
-            ways *= euler.choose(balls_per_color, count)
-        num_colors_to_count[num_colors_for_set] += ways
+        assert sum(color_to_count.values()) == NUM_BALLS_TO_PICK
+        num_colors_for_set = len(filter(lambda x: x > 0, color_to_count.values()))
+        ways = reduce(lambda accumulator, new_value: euler.choose(BALLS_PER_COLOR, new_value) * accumulator,
+                      color_to_count.values(), 1)
+        num_colors_to_ways[num_colors_for_set] += ways
         return
     if colors_left <= 0:
         return
 
-    for n in xrange(balls_per_color + 1):
-        color_idx = num_colors - colors_left
-        acc[color_idx] = n
-        rcr(colors_left - 1, balls_left - n, acc)
-        acc[color_idx] = 0
+    for n in xrange(BALLS_PER_COLOR + 1):
+        color_idx = NUM_COLORS - colors_left
+        color_to_count[color_idx] = n
+        rcr(colors_left - 1, balls_left - n, color_to_count, num_colors_to_ways)
+        color_to_count[color_idx] = 0
 
-rcr(num_colors, max_pick, {}, 0)
+def solve():
+    num_colors_to_ways = Counter()
+    rcr(NUM_COLORS, NUM_BALLS_TO_PICK, {}, num_colors_to_ways)
 
-s2 = reduce(lambda y, x: x[0] * x[1] + y, num_colors_to_count.items(), 0)
+    s2 = reduce(lambda y, x: x[0] * x[1] + y, num_colors_to_ways.items(), 0)
+    print float(s2) / euler.choose(70, 20)
 
-print float(s2) / euler.choose(70, 20)
+if __name__ == '__main__':
+    solve()
